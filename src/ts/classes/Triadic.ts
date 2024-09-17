d7a89ec0826b4d9f135a3301efc70f678c2c4c4c
Karin Silfversparre <ks224ac@student.lnu.se>
2024-09-17 11:30:31 +0200

    Add class Triadic

---
+++ src/ts/classes/Triadic.ts
@@ -0,0 +1,33 @@
+/**
+ * @file Module for the class Triadic.
+ * @module src/ts/classes/Triadic
+ * @author Karin Silfversparre <ks224ac@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// import { ColorTheme } from './ColorTheme.js'
+import { ColorSchemes } from './ColorSchemes.js'
+import { Color } from './Color.js'
+import { MultiHueColorTheme } from './MultiHueColorTheme.js'
+
+/**
+ * Represents a triadic color theme.
+ */
+export class Triadic extends MultiHueColorTheme {
+  /**
+   * Creates a new Triadic object.
+   */
+  constructor () {
+    super()
+  }
+
+  /**
+   * Generates a color theme.
+   *
+   * @param {number} numberOfColors - The number of colors to include ranging from 3 to 5.
+   * @throws {Error} The number of colors must be between 3 and 5.
+   * @returns {object} An object containing data about the generated color theme.
+   */
+  generateColorTheme (numberOfColors:number): {numberOfColors: number, colorScheme: ColorSchemes, colors: Color[]} {
+  }
+}
