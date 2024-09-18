f3305577786a0e8baed77229bafb7a81d1e5b1bc
Karin Silfversparre <ks224ac@student.lnu.se>
2024-09-18 13:14:54 +0200

    Replace arguments in ColorTheme and MultiHueColorTheme constructors with enums

--- src/ts/classes/MultiHueColorTheme.ts
+++ src/ts/classes/MultiHueColorTheme.ts
@@ -6,6 +6,7 @@
  */
 
 import { ColorTheme } from "./ColorTheme.js"
+import { ColorValues } from "./ColorValues.js"
 
 /**
  * Represents a multi hue color theme.
@@ -17,6 +18,33 @@ export abstract class MultiHueColorTheme extends ColorTheme {
    * @type {number[]}
    */
   protected hues: number[]
+
+  /**
+   * The lightness of the color theme.
+   *
+   * @type {number}
+   */
+  protected lightness: number
+
+  /**
+   * Creates a new MultiHueColorTheme object.
+   *
+   * @class
+   */
+  constructor () {
+    super()
+    this.#setLightness(ColorValues.LightnessMax, ColorValues.LightnessMin)
+  }
+
+  /**
+   * Sets the lightness.
+   *
+   * @param {number} maxValue - The maximum value lightness can be set to.
+   * @param {number} minValue - The minimum value lightness can be set to.
+   */
+  #setLightness (maxValue:number, minValue:number) {
+    this.lightness = this.generateRandomNumber(maxValue, minValue)
+  }
 
   /**
    * Picks a random hue from an array.
--- src/ts/classes/Triadic.ts
+++ src/ts/classes/Triadic.ts
@@ -29,5 +29,30 @@ export class Triadic extends MultiHueColorTheme {
    * @returns {ColorThemeData} An object containing data about the generated color theme.
    */
   generateColorTheme (numberOfColors:number): ColorThemeData {
+    if (numberOfColors < 3 || numberOfColors > 5) {
+      const error = new Error('The number of colors must be between 3 and 5.')
+      // error.status = 400
+      throw error
+    }
+
+    // const colors: Color[] = []
+    // const colorTheme = {
+    //   numberOfColors,
+    //   colorScheme: ColorSchemes.Monochrome,
+    //   colors
+    // }
+
+    for (let i = 0; i < numberOfColors; i++) {
+      // MAGIC NUMBER
+      const lightnessIncrement = (this.maxLightness - this.minLightness) / (numberOfColors - 1) // 1 since the number of increments is one less than number of colors.
+      const calculatedLightness = this.minLightness + (lightnessIncrement * i)
+      const saturation = this.adjustNumber(this.saturation, 10) // 10 for slight variation.
+
+      const color = new Color(this.hue, saturation, calculatedLightness)
+
+      colorTheme.colors.push(color)
+    }
+
+    return colorTheme
   }
 }
