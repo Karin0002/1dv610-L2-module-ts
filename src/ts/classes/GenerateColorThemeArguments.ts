806f3cd8d8330b4718324acd0f72abc79bd28bbb
Karin Silfversparre <ks224ac@student.lnu.se>
2024-09-23 15:42:52 +0200

    Add enum for argument limitation for generateColorTheme

---
+++ src/ts/classes/GenerateColorThemeArguments.ts
@@ -0,0 +1,13 @@
+// Very long name.
+export enum GenerateColorThemeArgumentLimits {
+  AnalogousMax = 5,
+  AnalogousMin = 3,
+  ComplementaryMax = 4,
+  ComplementaryMin = 2,
+  MonochromeMax = 5,
+  MonochromeMin = 2,
+  SplitComplementaryMax = 5,
+  SplitComplementaryMin = 3,
+  TriadicMax = 5,
+  TriadicMin = 3
+}
