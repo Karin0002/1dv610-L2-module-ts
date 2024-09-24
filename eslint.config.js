59a160decc4cf80630cdd06d6a79b7b9d44e4bed
Karin Silfversparre <ks224ac@student.lnu.se>
2024-09-24 11:31:12 +0200

    Edit eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
 import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
 import tsdoc from "eslint-plugin-tsdoc";
 import tsParser from "@typescript-eslint/parser";
-// import path from "node:path";
-// import { fileURLToPath } from "node:url";
-// import js from "@eslint/js";
-// import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//     baseDirectory: __dirname,
-//     recommendedConfig: js.configs.recommended,
-//     allConfig: js.configs.all
-// });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
 export default [
-    // ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
     {
         files: ["**/*.ts"],
         plugins: {
@@ -29,8 +29,7 @@ export default [
             sourceType: "module",
             parserOptions: {
                 project: "./tsconfig.json",
-                // tsconfigRootDir: new URL('.', import.meta.url).pathname,
-                tsconfigRootDir: "C:\\Users\\Karin\\Desktop\\1dv610\\1dv610-L2-module-ts",
+                tsconfigRootDir: __dirname
             },
         },
         rules: {
