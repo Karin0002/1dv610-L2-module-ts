e2e73fd51a82016258a1cd4e8e0adef28d4126da
Karin Silfversparre <ks224ac@student.lnu.se>
2024-10-01 09:46:45 +0200

    Add documentation for public interface

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,16 @@ try {
     console.log(error.message)
   }
 }
+// # RENAME APP.TS TO INDEX.TS
+
+// # IN INDEX.TS:
+// ## export * from './classes/Analogous.js'
+// ## export * from './classes/Complementary.js'
+// ## export * from './classes/CSSColorSetter.js'
+// ## export * from './classes/HTMLColorSwatch.js'
+// ## export * from './classes/Monochrome.js'
+// ## export * from './classes/RandomColorTheme.js'
+// ## export * from './classes/SplitComplementary.js'
+// ## export * from './classes/Triadic.js'
+
+// # CHANGE PACKAGE JSON SO IT IS CORRECT REGARDING FACTS AND NPM
