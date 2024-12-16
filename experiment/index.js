be4ec536bf98f0e135b304e41ef3b7798442ac07
Karin Silfversparre <ks224ac@student.lnu.se>
2024-12-16 15:20:50 +0100

    Remove unused dirs

--- experiment/index.js
+++
@@ -1,9 +0,0 @@
-import { Triadic, HTMLColorSwatch } from 'color-theme-generator'
-
-const triadicGenerator = new Triadic()
-const generatedTheme = triadicGenerator.generateColorTheme(3)
-
-const swatchGenerator = new HTMLColorSwatch()
-const generatedHTML = swatchGenerator.turnElementIntoColorSwatch(generatedTheme.colorsInTheme[0])
-
-console.log(generatedHTML)
